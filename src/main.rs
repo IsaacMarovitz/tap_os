bd6177aa0760120e55e450c777ebba3f451070f7
Isaac Marovitz <42140194+IsaacMarovitz@users.noreply.github.com>
2023-03-07 22:01:23 +0000

    Who likes printing?

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,16 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    println!("Welcome to TapOS!");
+    println!();
+    print!("The answer to life, the universe and everything is {}.", "42");
+
+    print!("According to all known laws of aviation, there is no way a bee should be able to fly.\nIts wings are too small to get its fat little body off the ground.\nThe bee, of course, flies anyway because bees don't care what humans think is impossible.\nYellow, black. Yellow, black. Yellow, black. Yellow, black.\nOoh, black and yellow!\nLet's shake it up a little.");
+
     loop {}
 }
 
