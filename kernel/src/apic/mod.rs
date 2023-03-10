4730cf4feca80a86590478c4c5fd24a4764ec95b
Isaac Marovitz <42140194+IsaacMarovitz@users.noreply.github.com>
2023-03-10 16:18:28 +0000

    bro idk anymore

--- kernel/src/apic/mod.rs
+++ kernel/src/apic/mod.rs
@@ -1,6 +1,8 @@
 use acpi::{platform::interrupt::IoApic};
 use alloc::vec::Vec;
 
+use crate::apic::io_apic::IoApicBase;
+
 extern crate bit_field;
 extern crate volatile;
 
@@ -11,6 +13,8 @@ pub fn init(io_apics: Vec<IoApic>) {
     log::info!("Init APIC, found {} IOAPICs", io_apics.iter().count());
     for io_apic in io_apics.iter() {
         log::info!("IOAPIC id: {}, address: {}, GSIB: {}", io_apic.id, io_apic.address, io_apic.global_system_interrupt_base);
-        
+        let base_addr: *mut u8 = &mut (io_apic.address as u8);
+        let mut io_apic_base = unsafe { IoApicBase::new(base_addr) };
+        log::info!("Uhh {:#?}", io_apic_base.read_redirection_table_entry(0).delivery_mode());
     }
 }
