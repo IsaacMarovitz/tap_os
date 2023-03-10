d8f7448a747b560e00a94195869c05210c54bc94
Isaac Marovitz <42140194+IsaacMarovitz@users.noreply.github.com>
2023-03-10 16:31:39 +0000

    idk if im doing this right but i think its progress

--- kernel/src/apic/mod.rs
+++ kernel/src/apic/mod.rs
@@ -1,7 +1,10 @@
 use acpi::{platform::interrupt::IoApic};
 use alloc::vec::Vec;
+use log::info;
 
-use crate::apic::io_apic::IoApicBase;
+use crate::apic::{io_apic::IoApicBase};
+
+use self::values::RedirectionTableEntry;
 
 extern crate bit_field;
 extern crate volatile;
@@ -15,6 +18,11 @@ pub fn init(io_apics: Vec<IoApic>) {
         log::info!("IOAPIC id: {}, address: {}, GSIB: {}", io_apic.id, io_apic.address, io_apic.global_system_interrupt_base);
         let base_addr: *mut u8 = &mut (io_apic.address as u8);
         let mut io_apic_base = unsafe { IoApicBase::new(base_addr) };
-        log::info!("Uhh {:#?}", io_apic_base.read_redirection_table_entry(0).delivery_mode());
+        io_apic_base.read_redirection_table_entry(0).set_masked(true);
+        io_apic_base.update_redirection_table_entry(0, test);
     }
+}
+
+fn test(value: &mut RedirectionTableEntry) {
+    log::info!("Hello from IRQ 1");
 }
